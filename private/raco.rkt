f57546369bcd26fd3549261b1128bb6607bd51ff
Ryan Culpepper <ryanc@racket-lang.org>
2022-05-03 19:01:04 +0200

    add "--print" flag for printing pkg names instead of paths

--- private/raco.rkt
+++ private/raco.rkt
@@ -1,6 +1,9 @@
 #lang racket/base
 (require racket/cmdline
+         racket/list
          raco/command-name
+         pkg/path
+         setup/main-collects
          "../main.rkt"
          (only-in "whereis.rkt"
                   whereis-system-base-keys
@@ -14,25 +17,50 @@
 ;;   - 2 if any request returned #f (but not empty list)
 ;;   - 0 otherwise
 
+(define (result->paths v)
+  (cond [(list? v) (append* (map result->paths v))]
+        [(path? v) (list (simplify-path v))]
+        [(string? v) (list (simplify-path (string->path v)))]))
+
 (define (report v [prefix ""])
   (define (print-path p)
     (printf "~a~a\n" prefix (path->string (simplify-path p))))
-  (cond [(list? v) (for-each print-path v)]
-        [(path? v) (print-path v)]
-        [(string? v) (printf "~a~a\n" v)]))
+  (for-each print-path (result->paths v)))
+
+;; report-pkg : ... -> Boolean, #f if any path could not be mapped to pkg
+(define (report-pkg v)
+  (define pkgs
+    (for/list ([p (in-list (result->paths v))])
+      (or (path->pkg/base p)
+          (begin0 #f (eprintf "failed to find pkg for ~e\n" (path->string p))))))
+  (for ([pkg (in-list (remove-duplicates (filter string? pkgs)))])
+    (printf "~a\n" pkg))
+  (andmap values pkgs))
+
+(define (path->pkg/base p)
+  (or (path->pkg p)
+      (let ([r (path->main-collects-relative p)])
+        (if (and (pair? r) (eq? (car r) 'collects)) "base" #f))))
 
 (define (string->datum what s)
   (define in (open-input-string s))
   (begin0 (read in)
     (unless (eof-object? (peek-char in))
-      (raise-user-error '|raco whereis|
-                        "expected one S-expression for ~a, given: ~s"
-                        what s))))
+      (whereis-error "expected one S-expression for ~a, given: ~s" what s))))
+
+(define (whereis-error fmt . args)
+  (apply raise-user-error '|raco whereis| fmt args))
 
 (define (raco-whereis-command args)
 
   ;; The code below is written to allow list of requests, but
   ;; currently limited to one (or zero) by #:once-any.
+
+  (define show 'path)
+
+  (define (check-show-path mode)
+    (unless (eq? show 'path)
+      (whereis-error "cannot use ~a mode with --print option" mode)))
 
   (define todo null) ;; (Listof (-> (U Path (Listof Path))))
   (define (push! proc [fail void])
@@ -46,50 +74,52 @@
 
    [("-a" "--auto")
     module-or-collection-or-pkg
-    ["" "Print the most relevant associated *directory* (not file) path"]
-    (push! (lambda () (whereis-auto module-or-collection-or-pkg)))]
+    ["Print the most relevant associated *directory* (not file) path"]
+    (push! (lambda ()
+             (check-show-path "--auto")
+             (whereis-auto module-or-collection-or-pkg)))]
 
    [("-m" "--module")
     module-path
-    "Print the path of the given module"
+    "Print the location of the given module"
     (let ([modpath (string->datum "module path" module-path)])
       (push! (lambda () (whereis-module modpath))))]
 
    [("-l" "--library-module")
     lib-module-path
-    ["" "Print the path of `(lib ,lib-module-path)"]
+    ["Print the location of `(lib ,lib-module-path)"]
     (push! (lambda () (whereis-module `(lib ,lib-module-path))))]
 
    [("-p" "--pkg")
     package
     "Print the package's directory"
-    (push! (lambda () (whereis-pkg package)))]
+    (push! (lambda () (begin (check-show-path "--pkg") (whereis-pkg package))))]
 
    [("-c" "--collect")
     collection
-    "Print the collection's directories"
+    "Print the collection's locations"
     (push! (lambda () (whereis-collection collection)))]
 
    [("-s" "--system")
     location
-    "Print the location's path or paths"
+    "Print the named location's path or paths"
     (let ([location (string->symbol location)])
-      (push! (lambda () (whereis-system location))))]
+      (push! (lambda () (begin (check-show-path "--system") (whereis-system location)))))]
 
    [("-r" "--raco")
     command
-    "Print the path of the raco command's implementation"
+    "Print the location of the raco command's implementation"
     (push! (lambda () (whereis-raco command)))]
 
    [("-b" "--binding")
     providing-module name
-    ["" "Print the path where the given name was defined"]
+    ["Print the location where the given name was defined"]
     (let ([providing-module (string->datum "module path" providing-module)]
           [name (string->symbol name)])
       (push! (lambda () (whereis-binding/symbol providing-module name))))]
 
    [("--all-system-paths")
-    "Show all supported --system keys and their values"
+    "Show all supported --system keys and their values."
     (let ()
       (define (show-keys keys)
         (for ([sym (in-list keys)])
@@ -101,6 +131,18 @@
       (show-keys whereis-system-base-keys)
       (show-keys (map car whereis-system-procs)))]
 
+   #:once-each
+   [("--print")
+    mode
+    ["Determines what kind of location is printed."
+     "  If <mode> is path (the default), print location paths."
+     "  If <mode> is pkg, print each location's corresponding package instead."]
+    (case mode
+      [("path") (set! show 'path)]
+      [("pkg") (set! show 'pkg)]
+      [else (raise-user-error '|raco whereis|
+                              "expected expected either `path` or `pkg` for --print option")])]
+
    #:args ()
    (let ([any-failed? #f])
      (for ([proc (in-list (reverse todo))])
@@ -109,7 +151,9 @@
                           (set! any-failed? #t)
                           (define msg (exn-message e))
                           (eprintf "~a\n" (exn-message e)))])
-         (report (proc))))
+         (case show
+           [(path) (report (proc))]
+           [(pkg) (or (report-pkg (proc)) (set! any-failed? #t))])))
      (exit (if any-failed? 2 0)))))
 
 ;; ============================================================
